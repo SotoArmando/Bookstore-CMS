09f5a42b666f0b56dc3666f9c65eb671edce19ca
SotoArmando <armando29@live.com>
2020-03-21 01:05:37 -0400

    feature/components added + components approach + redux actions & reducers + redux store

---
+++ src/Components/booksform.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+booksform = props => {
+    const categories = ["Action", "Biography", "History", "Horror", "Kids", "Learning", "Sci-Fi"];
+    return (
+        <div className="App">
+        </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './Components/app';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './Reducers/index'
+import App from './components/app'
+import './index.css'
+
+const books = [
+    { "id": Math.random * 99999, "name": "The greay history", "desc": "This is a big book", "categorie": 0 },
+    { "id": Math.random * 99999, "name": "The greay history 1", "desc": "This is a big book 1", "categorie": 1 },
+]
+
+const store = createStore(reducers, { books })
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
